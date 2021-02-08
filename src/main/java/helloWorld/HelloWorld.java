2684d2615d0eb0c1a76c7874e2556fa2a72cfce1
adamsmanuel <adams5464@gmail.com>
2021-02-08 11:09:40 +0100

    Finish first exercise

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -3,17 +3,33 @@ package helloWorld;
 import processing.core.PApplet;
 
 public class HelloWorld extends PApplet {
-
-    public void settings() {
-        size(600, 600);
-    }
-
-    public void draw() {
-        background(0);
-        ellipse(mouseX, mouseY, 20, 20);
-    }
+    private float rnd;
 
     public static void main(String... args) {
         PApplet.main("helloWorld.HelloWorld");
+    }
+
+    public void settings() {
+        size(900, 600);
+    }
+
+    public void setup() {
+        surface.setResizable(true);
+        background(83, 153, 245);
+        frameRate(100);
+    }
+
+    public void draw() {
+        rnd = random(10, 40);
+        var color = color(255, random(0, 100), random(0, 100), 200);
+
+        ellipseMode(RADIUS);
+        fill(color);
+        noStroke();
+        ellipse(random(width), random(height), rnd, rnd);
+    }
+
+    public void mousePressed() {
+        saveFrame("output-" + rnd + ".png");
     }
 }
