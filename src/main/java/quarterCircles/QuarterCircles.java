f1cd0f0f0b99aff2ffea7792dcced18fd6332066
adamsmanuel <adams5464@gmail.com>
2021-11-09 12:38:47 +0100

    finished task quarterCircles

--- src/main/java/quarterCircles/QuarterCircles.java
+++ src/main/java/quarterCircles/QuarterCircles.java
@@ -3,25 +3,56 @@ package quarterCircles;
 import processing.core.PApplet;
 
 public class QuarterCircles extends PApplet {
+    private final int WIDTH = 32;
+    private final int HEIGHT = 32;
 
     public static void main(String... args) {
         PApplet.main("quarterCircles.QuarterCircles");
     }
 
     public void settings() {
-        size(600, 600);
+        size(1024, 1024);
     }
 
     public void setup() {
         background(255);
-        frameRate(100);
+        singleDraw();
     }
 
-    public void draw() {
-
+    public void singleDraw() {
+        for (int x = 0; x <= 1024; x = x + WIDTH) {
+            for (int y = 0; y <= 1024; y = y + HEIGHT) {
+                fillHalfCircles((int) random(0, 5), x, y);
+            }
+        }
     }
 
-    public void mousePressed() {
-        saveFrame("HelloWorld.png");
+    void fillHalfCircles(int random, int x, int y) {
+        stroke(255);
+        fill((int) random(0, 256),
+                (int) random(0, 256),
+                (int) random(0, 256));
+        switch (random) {
+            case 0:
+                drawHalfCircle(x, y, 0, HALF_PI);
+                break;
+            case 1:
+                drawHalfCircle(x + WIDTH, y, HALF_PI, PI);
+                break;
+            case 2:
+                drawHalfCircle(x + WIDTH, y + HEIGHT, PI, PI + HALF_PI);
+                break;
+            case 3:
+                drawHalfCircle(x, y + HEIGHT, PI + HALF_PI, PI + PI);
+                break;
+            case 4:
+                break;
+            default:
+                throw new IllegalArgumentException("Argument out of bounds");
+        }
+    }
+
+    void drawHalfCircle(float x, float y, float start, float stop) {
+        arc(x, y, WIDTH * 2, HEIGHT * 2, start, stop, PIE);
     }
 }
