f853b3a97d0f8094e35ed84a1865f203b05e274e
adamsmanuel <adams5464@gmail.com>
2021-11-20 14:46:30 +0100

    finish task

--- src/main/java/translateRotateQuads/TranslateRotateQuads.java
+++ src/main/java/translateRotateQuads/TranslateRotateQuads.java
@@ -2,6 +2,8 @@ package translateRotateQuads;
 
 import processing.core.PApplet;
 
+import java.util.Random;
+
 public class TranslateRotateQuads extends PApplet {
 
     public static void main(String... args) {
@@ -21,16 +23,18 @@ public class TranslateRotateQuads extends PApplet {
         background(255);
         randomSeed(36421);
         translate((width - 32 * 12) / 2f, (height - 32 * 24) / 2f);
+        float rotationFactor = sin((float) (0.0001 * millis()));
+        float translationFactor = 0.0001f * millis();
+
         for (int x = 0; x < 12; x++) {
             for (int y = 0; y < 24; y++) {
-                float rotationFactor = sin((float) (0.0001 * millis()));
-                float translationFactor = 0.0001f * millis();
                 pushMatrix();
                 translate(x * 32, y * 32);
                 rotate(radians(y * rotationFactor * (random(-20, 20))));
                 translate(x * (translationFactor * random(-1, 1)), y * (translationFactor * random(-1, 1)));
                 rect(0, 0, 32, 32);
                 popMatrix();
+
             }
         }
     }
